d3dd702239cdeef5571d43c6100cec81f97dd3bc
Andrii Zakharchenko <andrii.zakharchenko@effem.com>
2024-09-23 15:31:11 +0200

    modue-3

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,147 @@
-const os = require('os');
+type TypeListener = Record<string, {
+  event: Function,
+  once?: Boolean
+}[]>;
 
-import { exec, ExecException } from 'child_process';
-import logProcesses from './activityMonitor';
+class EventEmitter {
+  protected listeners: TypeListener = {}
 
-let lastOutput = '';
+  addListener(eventName: string, fn: Function, once?: boolean) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
 
-function checkPlatform(): string {
-  const platform = os.platform();
-  const unixLikePlatforms = [
-    'aix',       // IBM AIX
-    'darwin',    // macOS (і iOS)
-    'freebsd',   // FreeBSD
-    'linux',     // Linux дистрибутиви
-    'openbsd',   // OpenBSD
-    'netbsd',    // NetBSD
-    'sunos'      // SunOS (Solaris)
-  ];
-  const windowsPlatforms = [
-    'win32',     // Windows (усі версії)
-    'cygwin'     // Cygwin (POSIX-спільний шар для Windows)
-  ];
+    this.listeners[eventName].push({
+      event: fn,
+      once
+    });
+  }
 
-  if (unixLikePlatforms.includes(platform)) {
-    return 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1';
-  } else if (windowsPlatforms.includes(platform)) {
-    return `powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + ' ' + $_.CPU + ' ' + $_.WorkingSet }"`;
-  } else {
-    logProcesses(`Unsupported platform - ${os.platform()}`, 'activityMonitor.log');
-    process.stdout.end('Process exited with code 1 ');
-    process.exit(1);
+  on(eventName: string, fn: Function) {
+    this.addListener(eventName, fn);
+  }
+
+  emit(eventName: string, ...args: any[]) {
+    if (this.listeners.hasOwnProperty(eventName)) {
+      let updatedListeners = [];
+
+      for (const listener of this.listeners[eventName]) {
+        listener.event.call(this.listeners[eventName], args);
+
+        if (!listener.once) {
+          updatedListeners.push(listener);
+        }
+      }
+
+      this.listeners[eventName] = updatedListeners;
+    } else {
+      console.error(`Event ${eventName} was not found`);
+    }
+  }
+
+  removeListener(eventName: string, fn: Function) {
+    if (this.listeners.hasOwnProperty(eventName)) {
+      delete this.listeners[eventName];
+    }
+
+    console.error(`Event ${eventName} was not found`);
+  }
+
+  off(eventName: string, fn: Function) {
+    this.removeListener(eventName, fn);
+  }
+
+  once(eventName: string, fn: Function) {
+    this.addListener(eventName, fn, true);
+  }
+
+  listenerCount(eventName: string) {
+    if (this.listeners.hasOwnProperty(eventName)) {
+      return Object.keys(this.listeners[eventName]).length;
+    } else {
+      return 0;
+    }
+  }
+
+  rawListeners(eventName: string) {
+    if (this.listeners.hasOwnProperty(eventName)) {
+      return this.listeners[eventName].map((listener) => listener.event);
+    } else {
+      return [];
+    }
   }
 }
 
-function execProcess(command: string) {
-  exec(command, (error: ExecException | null, stdout: string, stderr: string) => {
-    if (error) {
-      logProcesses(`Error: ${error.message}`, 'activityMonitor.log');
-      process.stdout.write('Process exited with code 1 ');
-      return;
-    }
+// const emitter = new EventEmitter();
+//
+// // Test addListener and emit
+// emitter.addListener('testEvent', (data: any) => console.log(data));
+// emitter.emit('testEvent', 'Hello, World!'); // Should log 'Hello, World!'
+//
+// // Test listenerCount
+// console.log(emitter.listenerCount('testEvent')); // Should log 1
+//
+// // Test rawListeners
+// console.log(emitter.rawListeners('testEvent')); // Should log [ [Function] ]
+//
+// // Test removeListener
+// emitter.removeListener('testEvent', console.log);
+// console.log(emitter.listenerCount('testEvent'));
 
-    if (stderr) {
-      logProcesses(stderr, 'activityMonitor.log');
-      process.stdout.write('Process exited with code 1 ');
-      return;
-    }
+// Метод executeповинен видавати такі події:
+//
+//   beginподія на початку виконання.
+//   dataподія, якщо асинхронна функція надає дані, передаючи дані як аргумент будь-яким dataслухачам подій.
+//   endподія в кінці виконання.
+//   errorподія, якщо під час виконання асинхронної функції виникає помилка, передаючи об’єкт Error як аргумент будь-яким errorслухачам подій.
 
-    lastOutput = stdout;
-  });
+// class withTime extends EventEmitter {
+//   execute(eventName: string, fn: Function) {
+//     console.time()
+//     this.addListener(eventName, fn);
+//   }
+// //   dataподія, якщо асинхронна функція надає дані, передаючи дані як аргумент будь-яким dataслухачам подій.
+// //   endподія в кінці виконання.
+// //   errorподія, якщо під час виконання асинхронної функції виникає помилка, передаючи об’єкт Error як аргумент будь-яким errorслухачам подій.
+//
+// }
+
+const csv = require('csvtojson');
+import fs from 'fs';
+
+// function exportCsvToTxt(csvFilePath: string) {
+//   return csv().fromFile(csvFilePath)
+//     .then(async (json: any) => {
+//       const txtFilePath = csvFilePath.replace('csv', 'txt');
+//
+//       fs.writeFile(txtFilePath, JSON.stringify(json), (error: any) => {
+//         if (error) throw error;
+//
+//         console.log(`${txtFilePath} was created successfully.`);
+//       });
+//     })
+//     .catch((error: any) => {
+//       console.error(error);
+//     });
+// }
+
+
+function exportCsvToTxt(csvFilePath: string): void {
+  const txtFilePath = csvFilePath.replace('.csv', '.txt');
+  const readableStream = fs.createReadStream(csvFilePath);
+  const writableStream = fs.createWriteStream(txtFilePath);
+
+  readableStream
+    .pipe(csv())
+    .on('data', (chunk: Buffer) => {
+      writableStream.write(chunk.toString('utf-8'));
+    })
+    .on('end', () => {
+      console.log('Operation csv to txt successfully finished!');
+    })
+    .on('error', (error: any) => {
+      console.log('Operation csv to txt failed!', error);
+    });
 }
 
-
-function updateConsole() {
-  console.clear();
-  console.log(lastOutput);
-}
-
-function updateLog() {
-  logProcesses(lastOutput, 'activityMonitor.log');
-}
-
-setInterval(() => {
-  const command = checkPlatform();
-  execProcess(command);
-}, 100);
-
-setInterval(updateConsole, 100);
-setInterval(updateLog, 60000);
-
-
+exportCsvToTxt('src/assets/books.csv');
